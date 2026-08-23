be38e1ec96c91ec5b3ee1ba8c9b2d15ebb67af4c
practical-problems <practicalpr0blems@outlook.com>
2026-08-23 15:29:32 -0400

    Fix bugs  - raise exceptions instead of just strings  - fix typo in to_file  - fix crs comparison to "" due to crs being an object  - fix reproject throwing away the new projection  - fix toString method (JS) to str(self.fileLocation)

--- scripts/geoFile.py
+++ scripts/geoFile.py
@@ -13,15 +13,15 @@ class GeoFile:
 
     def save(self, save_file_name : str, save_file_type : str):
         if not self.loadStatus:
-            raise "There no file loaded in memory"
-        self.gdf.to_File(filename = save_file_name, driver = save_file_type)
+            raise ValueError("There no file loaded in memory")
+        self.gdf.to_file(filename = save_file_name, driver = save_file_type)
 
     def set_projection(self, projection_to_be_set_to : str):
-        if self.gdf.crs is not None or self.gdf.crs != "":
-            raise "There already an projection in " + self.fileLocation.toString
+        if self.gdf.crs is not None:
+            raise ValueError("There already an projection in " + str(self.fileLocation))
         self.gdf = self.gdf.set_crs(projection_to_be_set_to)
 
     def reproject(self, projection_to_be_set_to : str):
-        if self.gdf.crs is None or self.gdf.crs =="":
-            raise "There is no projections in the file!"
-        self.gdf.to_crs(projection_to_be_set_to)
+        if self.gdf.crs is None:
+            raise ValueError("There is no projections in the file!")
+        self.gdf = self.gdf.to_crs(projection_to_be_set_to)
